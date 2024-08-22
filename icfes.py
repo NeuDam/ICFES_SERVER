f9e4694607f3ce0f26f802f9aee61c5fae835b69
Alex A.V <109841257+NeuDam@users.noreply.github.com>
2024-08-22 18:02:18 -0500

    RESPONSE WITH NAME AND REGISTER OF EXAM IS WORKING

--- icfes.py
+++ icfes.py
@@ -16,13 +16,17 @@ class Icfes():
 
         r = requests.post(url=url, headers={"Content-Type": "application/json"}, json=post_data).json()
 
-        self.token = r["token"]
-        
-        self.get_name_student()
+        print(r)
+
+        try:
+            self.token = r["token"]
+        except KeyError:
+            return False, {"status": False, "message": "Error en datos ingresados"}
 
         data = [{"numeroRegistro": test["numeroRegistro"], "examen": test["datosParametros"]["examen"], "periodo": test["datosParametros"]["periodoAnioExamen"]} for test in r["datosAutenticacion"]]
 
-        return data
+        self.get_name_student(data)
+        return True, data
     
     def get_name_student(self, data):
         url = f"https://resultadosbackendoci.icfes.edu.co/api/datos-basicos/datosBasicosRespuesta?examen={data[0]['examen']}&identificacionUnica={data[0]['numeroRegistro']}"
@@ -36,18 +40,31 @@ class Icfes():
 
         r = requests.get(url=url, headers={"Content-Type": "application/json", "Authorization": self.token}).json()
 
-        return r
 
-    async def query_test(self):
-        
-        final_data = list()
-
-        first_response = await self.auth_request()
-
-        for exam in first_response:
-            final_data.append(await self.get_score_student(data=exam))
+        final_data = {
+            "ACREGISTRO": data["numeroRegistro"],
+            "puntaje": r["resultadosGenerales"]["puntajeGlobal"],
+            "mensajeMotivacional": r["resultadosGenerales"]["descripcionPercentilNacional"],
+            "puntajeMaterias": [ {"code": y["codigoPrueba"], "nombrePrueba": y["nombrePrueba"], "puntaje": y["puntajePrueba"], "nivel": y["ponderacionPrueba"]} for y in r["consultarPuntaje"]["puntajes"]]
+        }
 
         return final_data
-        
 
-    
+    def query_test(self):
+
+        final_data = list()
+
+
+        success_request, first_response = self.auth_request()
+
+        if not success_request:
+            return first_response
+        
+        for exam in first_response:
+            final_data.append(self.get_score_student(data=exam))
+
+        return {
+            "status": True,
+            "estudiante": self.name,
+            "examenes":final_data
+        }
